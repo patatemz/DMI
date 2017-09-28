5016d03ad538ba0c53b4178bcf4754ab53a65d0d
rodzhers69@gmail.com <patatemx@gmail.com>
2017-09-28 09:51:57 +0300

    20170928_09_50

---
+++ shell_examples.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#4.piemērs
+echo "Atbilde uz jautājumu - kas šobrīd tiek pildīts?"$0
+#echo $n
+echo "Skriptam nododu argumentu skaits: " $#
+
+
+#3. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+#unset NAME
+#echo $NAME
+
+#2. piemērs
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME="Uzvārds Vārds"
+#echo $NAME
+
+#1. piemērs
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+
