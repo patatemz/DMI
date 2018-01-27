aad9e09976a15b7e995c535e72decae5de8232a4
Artjoms Ivanovs <patatemx@gmail.com>
2018-01-27 04:20:31 +0200

    2018_01_27

--- FINAL/dichatomy.py
+++ FINAL/dichatomy.py
@@ -1,29 +1,33 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
-def mans_asinuss(x):
-    k = 0
-    a = 1*x**1/1
-    S = a
 
-    while k < 10:
-        k = k + 1
-        R = x**2*((2*k-1)**2)/(2*(2*k+1)*k)
-        a = a * R
-        S = S + a
+def mans_sinuss(x):
+    a=(-1)**0*x**1/(1)
+    S=a
+    f=500
+    k=0
+    n=1
+    while k<=f:
+        R=(-1)*x**2/((n*2)*(n*2+1))
+        a=a*R
+        S=S+a
+        n=n+1
+        k=k+1
     return S
 #zimesana
-a=0
-b=7
+a=1.57
+b=4.71
 x=np.arange(a,b,0.01)
-y=mans_asinuss(x)
+y=mans_sinuss(x)
 plt.plot(x,y)
 plt.grid()
 plt.show()
 #saknes meklesana
 delta_x=1.e-3
-funa=mans_asinuss(a)
-funb=mans_asinuss(b)
+funa=mans_sinuss(a)
+funb=mans_sinuss(b)
 if funa*funb>0:
     print("Starp [%.2f,%.2f] funkcijai nav saknes"%(a,b))
     print("vai funkcijai saja intervala ir paru saknu skaits, ko ar so funkciju nemekle")
@@ -33,7 +37,7 @@ k=0
 while b-a>delta_x:
     x=(a+b)/2
     k=k+1
-    funx=mans_asinuss(x)
+    funx=mans_sinuss(x)
     print("%3d. a=%.4f f(%.4f)=%7.4f b=(%.4f)"%(k,a,x,funx,b,))
     if funa*funx>0:
         a=x
