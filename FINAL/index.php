bda083eaac5cd7f74bb446280a6a7022f93fa708
patatemz <31726128+patatemz@users.noreply.github.com>
2018-01-28 00:30:26 +0200

    Add files via upload

--- FINAL/index.php
+++ FINAL/index.php
@@ -5,7 +5,7 @@
 	  <title>REBC03 Artjoms Ivanovs Datromacība</title>
 <style>
 	footer {
-		background-image: url("https://cdn.wallpapersafari.com/27/37/QXSf9a.jpg");
+		background-color: white;
 		background-position: 100% 100%;
 		background-repeat: repeat;
 	}
@@ -104,10 +104,10 @@ li2 {
    Mediāna;<br>
    Moda(s)
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+     <a href="https://github.com/patatemz/DMI/blob/master/LD0.sh" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD0.png?raw=true" /></a> 
     <p>
-      Apraksts:<br>
-	  
+     Apraksts:<br>
+	 Uzdevuma galvenais mērķis ir iemācīties lietot skaitļu masīvus un sarežģītākus ciklus
     </p>
 </div>
 <hr>
@@ -117,10 +117,10 @@ li2 {
    Uzdevumi:<br>
    Tabula ar atlikumiem no dalījuma
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD1.png?raw=true" /></a> 
     <p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis ir iemācīties ievades nolasi un pareizu izvades sintaksi
     </p>
 </div>
 <hr>
@@ -129,6 +129,7 @@ li2 {
     <p>
    Uzdevumi:
    <br>
+   
    Funkcijas aprēķins, izmantojot izvirzījumu Teilora rindā; <br>
    Funkcijas noteikšanas apgabala attēlošana; <br>
    Funkcijas izteiksmes zīmēšana, izmantojot ASCII simbolus; <br>
@@ -138,11 +139,12 @@ li2 {
    Pēdējā saskaitāma vērtības attēlošana; <br>
    Funkcijas vērtības attēlošana; <br>
    Summas vērtības attēlošana;
+   
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+     <a href="https://github.com/patatemz/DMI/blob/master/LD0.sh" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/ld2.png?raw=true" /></a> 
     <p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis ir iemācīties ekonomēt datora resursus, izmantojot matemātiski elegentākus risinājumus 
     </p>
 </div>
 <hr>
@@ -155,10 +157,18 @@ li2 {
    Funkcijas vērtības atrašanas vietas noteikšana ar dihotomijas metodi
 
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+     <a href="https://github.com/patatemz/DMI/blob/master/FINAL/dichatomy.py" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/dichatomy.png?raw=true" /></a>
+	 <img src="https://github.com/patatemz/DMI/blob/master/FINAL/dichatomy2.png?raw=true" />	 
+	<p>Klases darbs</p>
+	<br>
+	    </p>
+     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD3_11.png?raw=true" /></a>
+	 <img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD3_12.png?raw=true" />
+	 	<p>Individuālais darbs</p>
+	<br>
     <p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis iemācīt studentm, kā lietot dihotomijas metodi un zīmēt vienkāršus grafikus ar pyplot palīdzību.
     </p>
 </div>
 <hr>
@@ -170,10 +180,12 @@ li2 {
    Funkcijas pirmās kārtas atvasinājuma skaitliskā noteikšana un zīmēšana<br>
    Funkcijas otrās kārtas atvasinājuma skaitliskā noteikšana un zīmēšana<br>
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
-    <p>
+     <a href="https://github.com/patatemz/DMI/blob/master/FINAL/derrivative.py" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/derrivative.png?raw=true" /></a> 
+	<p>Klases darbs</p>
+	<br>
+	<p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis ir iemācīties lietot python, lai aprēķinātu vienkāršas matemātiskās funkcijas un attēlotu tās vienā grafikā
     </p>
 </div>
 <hr>
@@ -184,23 +196,31 @@ li2 {
    Gadījuma skaitļu ģenerēšana<br>
    Montekarlo metodes pielietojuma noteikšana
     </p>
-     <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+     <a href="https://github.com/patatemz/DMI/blob/master/FINAL/quiz.py" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/quiz.png?raw=true" /></a> 
+    <p>
+	<p>Klases darbs</p>
+	<br>
+	    </p>
+     <a href="https://github.com/patatemz/DMI/blob/master/FINAL/LD5.py" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD5.png?raw=true" /></a> 
+	 	<p>Individuālais darbs</p>
+	<br>
     <p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis ir iemācīties pielietot Montekarlo metodi matemātiskiem aprēķiniem.
     </p>
 </div>
 <hr>
 <div>
     <h2>#6 Labaratorijas darbs - Python: Kodēšana</h2>
 	<p>
-  Uzdevumi:<br>
-  Ziņojuma dekodēšana
+ Uzdevumi:<br>
+  Ziņojuma dekodēšana<br>
+  
 	 </p>  
-      <a href="http://github.com/patetmz/DMI" target="_blank"><img src="http://xmlwriter.net/images/screenshot.gif" /></a> 
+      <a href="https://github.com/patatemz/DMI/blob/master/darbi/L1C/test" target="_blank"><img src="https://github.com/patatemz/DMI/blob/master/FINAL/LD6.png?raw=true" /></a> 
    <p>
       Apraksts:<br>
-	  
+	  Uzdevuma galvenais mērķis ir iemācities kodēsanas/dekodēsanas pamatprincipus un iepazīties ar darbu caur remote serveri. Arī iemācīties ssh lietošanas pamatprincipus.
     </p>
 </div>
 
@@ -210,22 +230,22 @@ li2 {
     <h3>Nodarbību vēsture</h3>
 	</div>
 <ol>
-    <li><a href="...">mācību nedēļa (04.09 - 08.09)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history.txt">mācību nedēļa (11.09 - 15.09)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history_20170921_0950.txt">mācību nedēļa (18.09 - 22.09)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history20170928.txt">mācību nedēļa (25.09 - 29.09)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history_20171005_11_45">mācību nedēļa (02.10 - 06.10)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history_20171012.txt">mācību nedēļa (09.10 - 13.10)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history20171019_11_25.txt">mācību nedēļa (16.10 - 20.10)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history_20172610_9_45.txt">mācību nedēļa (23.10 - 27.10)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history20171102.txt">mācību nedēļa (30.10 - 03.11)</a></li>
-    <li><a href="https://github.com/patatemz/DMI">mācību nedēļa (06.11 - 10.11)bija aizmirsts par history</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history2017116.txt">mācību nedēļa (13.11 - 17.11)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history20171123.txt">mācību nedēļa (21.11 - 24.11)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/PYTHON/dienasgramata2017_11_30_11_31.py">mācību nedēļa (27.11 - 01.12)</a></li>
-    <li><a href="https://github.com/patatemz/DMI">mācību nedēļa(04.12 - 08.12)bija aizmirsts par history</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history20181214.txt">mācību nedēļa (11.12 - 15.12)</a></li>
-    <li><a href="https://github.com/patatemz/DMI/blob/master/history2017_21_12_09_50.txt">mācību nedēļa (18.12 - 22.12)</a></li>
+    <li><a target="_blank" href="...">mācību nedēļa (04.09 - 08.09)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history.txt">mācību nedēļa (11.09 - 15.09)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history_20170921_0950.txt">mācību nedēļa (18.09 - 22.09)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history20170928.txt">mācību nedēļa (25.09 - 29.09)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history_20171005_11_45">mācību nedēļa (02.10 - 06.10)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history_20171012.txt">mācību nedēļa (09.10 - 13.10)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history20171019_11_25.txt">mācību nedēļa (16.10 - 20.10)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history_20172610_9_45.txt">mācību nedēļa (23.10 - 27.10)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history20171102.txt">mācību nedēļa (30.10 - 03.11)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI">mācību nedēļa (06.11 - 10.11)bija aizmirsts par history</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history2017116.txt">mācību nedēļa (13.11 - 17.11)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history20171123.txt">mācību nedēļa (21.11 - 24.11)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/PYTHON/dienasgramata2017_11_30_11_31.py">mācību nedēļa (27.11 - 01.12)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI">mācību nedēļa(04.12 - 08.12)bija aizmirsts par history</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history20181214.txt">mācību nedēļa (11.12 - 15.12)</a></li>
+    <li><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/history2017_21_12_09_50.txt">mācību nedēļa (18.12 - 22.12)</a></li>
 </ol>
 <div>
   <h3>Tēmas</h3>
@@ -248,20 +268,20 @@ li2 {
       <dd><li1>git push origin master</li1></dd>
     <li>shell: shell interpretatora atrašanas vieta - #!/bin/bash</li>
     <li>shell: shell skriptu piemēri:</li>
-      <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/statistika>shell: skripta neliels apraksts - lietotajs ievada tris skaitljus</br> un programma pievieno tos masiva un velak tiek attelots tresais masiva elements.</a></li1></dd>
-      <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/dialogs.sh">shell: datu ievade/izvade</a></li1></dd>
-      <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/shell_operators.sh">shell: operācijas</a></li1></dd>
-      <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/if">shell: nosacījuma operators</a></li1></dd>
-	  <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/array_loop">shell: cikla operators</a></li1></dd>
+      <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/statistika">shell: skripta neliels apraksts - lietotajs ievada tris skaitljus</br> un programma pievieno tos masiva un velak tiek attelots tresais masiva elements.</a></li1></dd>
+	  <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/dialogs.sh">shell: datu ievade/izvade</a></li1></dd>
+      <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/shell_operators.sh">shell: operācijas</a></li1></dd>
+      <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/if">shell: nosacījuma operators</a></li1></dd>
+	  <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/array_loop">shell: cikla operators</a></li1></dd>
     <li>Python: Python interpretatora atrašanas vieta - usr/bin/python</li>
     <li>Python: vide ērtam darbam ar Python'u - idle</li>
     <li>Python: Python skriptu piemēri:</li>
-       <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/PYTHON/dialogs.py">Python: skripta neliels apraksts - lietotajs ievada </br> savu vardu un uzvardu un programma izvadis to uz ekrana.</a></li> </dd>   
-       <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/PYTHON/mainigie">Python: mainīgie</a></li1></dd>
-       <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/PYTHON/if.py">Python: nosacījuma operators</a></li1></dd>
-       <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/PYTHON/while">Python: cikla operators(i)</a></li1></dd>
-       <dd><li1><a href="">Python: lietotāja funkcija</a></li1></dd>
-       <dd><li1><a href="https://github.com/patatemz/DMI/blob/master/darbi/L1C/test">Python: darbs ar failiem</a></li1></dd>
+       <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/PYTHON/dialogs.py">Python: skripta neliels apraksts - lietotajs ievada </br> savu vardu un uzvardu un programma izvadis to uz ekrana.</a></li> </dd>   
+       <dd><li1><a target="_blank"a href="https://github.com/patatemz/DMI/blob/master/PYTHON/mainigie">Python: mainīgie</a></li1></dd>
+       <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/PYTHON/if.py">Python: nosacījuma operators</a></li1></dd>
+       <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/PYTHON/while">Python: cikla operators(i)</a></li1></dd>
+       <dd><li1><a target="_blank" href="">Python: lietotāja funkcija</a></li1></dd>
+       <dd><li1><a target="_blank" href="https://github.com/patatemz/DMI/blob/master/darbi/L1C/test">Python: darbs ar failiem</a></li1></dd>
    </ul>
             <div>
               <p1> &copy Rīgas Tehniskā universitāte 2018</p1>
